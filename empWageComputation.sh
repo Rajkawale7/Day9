9059e139c575bc6a44184e3f5220767f42d60212
Rajkawale7 <Rajkawale712@gmail.com>
2022-02-24 18:49:30 +0530

    Added: Daily Employee Wages

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -10,3 +10,33 @@ then
 else
      echo "Employee is Absent"
 fi
+
+
+#To calculate Daily Employee wages
+
+#Add Part time Employee & Wage
+
+WorkingHour=$(( RANDOM%3 ))
+
+#using switch case
+
+case $WorkingHour in
+
+     1) echo "FullTime"
+        work_hrs=8
+     ;;
+
+     2) echo "PartTime"
+        work_hrs=8
+     ;;
+
+     *) echo "FullTime + PartTime"
+        work_hrs=16
+     ;;
+
+esac
+#Calculate Daily Employee Wage
+
+emp_rate_per_hr=20
+daily_emp_wage=$(($empAttendance * $work_hrs * $emp_rate_per_hr))
+echo "Daily Employee Wage" $daily_emp_wage
